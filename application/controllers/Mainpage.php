f8836fea63e3357f3ba6a9e36ece38dcc58f2600
naveen <naveen@DESKTOP-UM3RRND>
2023-08-23 22:50:39 +0530

    testing

--- application/controllers/Mainpage.php
+++ application/controllers/Mainpage.php
@@ -12,7 +12,6 @@ class Mainpage extends MX_Controller
     
     public function index()
     {
-        //hello
         $data = array();
         // $data['css_links'] = 'includes/css_links';
         // $data['top_header'] = 'includes/top_header';
